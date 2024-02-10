2215e1841e79209eb4eaa93f0ad268582fb26bfc
Anton Butov <butov6101@gmail.com>
2024-02-10 14:57:27 +0300

    add parcel test

--- app/src/test/java/com/butovanton/objectstreamresearch/ParcelableTest.kt
+++ app/src/test/java/com/butovanton/objectstreamresearch/ParcelableTest.kt
@@ -1,6 +1,9 @@
 package com.butovanton.objectstreamresearch
 
 import android.os.Parcel
+import android.os.Parcelable
+import android.os.Parcelable.Creator
+import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
@@ -21,4 +24,38 @@ class ParcelableTest {
         parcel.writeString(string)
         println("dataSize = ${parcel.dataSize()}")
     }
+
+    data class MyClass(
+        val data: String
+    ) : Parcelable {
+        override fun describeContents(): Int {
+            return 0 //Parcelable.CONTENTS_FILE_DESCRIPTOR
+        }
+
+        override fun writeToParcel(dest: Parcel, flags: Int) {
+            dest.writeString(data)
+        }
+
+        companion object {
+            val creator = object : Creator<MyClass> {
+                override fun createFromParcel(source: Parcel): MyClass {
+                    return MyClass(source.readString() ?: throw IllegalArgumentException())
+                }
+
+                override fun newArray(size: Int): Array<MyClass> {
+                    return newArray(size)
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `pass myClass throw Intent`() {
+       val parcel = Parcel.obtain()
+       MyClass("data").writeToParcel(parcel, 0)
+       parcel.setDataPosition(0)
+       val newMyClass = MyClass.creator.createFromParcel(parcel)
+       Assert.assertEquals(newMyClass.data, "data")
+    }
 }
+
